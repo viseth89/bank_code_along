385f30424039113be26a13fe817eefe8df66759b
datalogics-vsen <viseth89@gmail.com>
2021-04-24 12:56:30 -0500

    Finished with HTML and CSS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,11 @@ button.onclick = (e) => {
     e.preventDefault()
     console.log("button")
 }
+
+
+
+let form = document.getElementById("transaction-form")
+form.onsubmit = (e) => {
+    console.log(e.target["transaction-type"].value)
+    console.log(e.target.amount.value)
+}
